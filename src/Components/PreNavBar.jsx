117452c360ba04f442dcb2d28ba0d6eaf6e47e60
developeragency5 <Developer.agency@yahoo.com>
2024-10-18 02:26:33 +0530

    done

---
+++ src/Components/PreNavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PreNavBar = () => {
+  return (
+    <header className="bg-black p-4">
+      <div className="container mx-auto text-white text-3xl font-bold">
+        MyCompany
+      </div>
+    </header>
+  );
+};
+
+export default PreNavBar;
